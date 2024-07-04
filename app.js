077b729538bb3d814350dc88c839c727c9debbd5
CristianFuertes97 <cristianfuertesguerrero@gmail.com>
2024-07-04 00:20:19 -0500

    Version final proyecto

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 let numeroSecreto = 0;
-let intentos = 1;
-
+let intentos = 0;
+let rango = 10;
+let numintentos = 5;
+console.log(intentos);
 function secreto() {
-    let rango = parseInt(document.getElementById('valorRango').value);
+    rango = parseInt(document.getElementById('valorRango').value);
     numeroSecreto = Math.floor(Math.random()*rango)+1;
+    mensaje('p',`Ingresa un numero entre el 1 al ${rango}`);
     console.log(numeroSecreto);
+
+    
+
     return numeroSecreto;
+}
+
+function condicionesIniciales() {
+    mensaje('p','Digita el rango a jugar');
+    mensaje('h1','Juego adivina el número');
+    intentos = 1;
+    return;
 }
 
 function mensaje(elemento, texto) {
@@ -17,17 +30,38 @@ function mensaje(elemento, texto) {
 function verificaIntento() {
     let numUsuario = parseInt(document.getElementById('valorUsuario').value);
     // console.log(numUsuario);
+    if(intentos > numintentos){
+        mensaje('h1','GAME OVER');
+        mensaje('p',`Has alcanzado el numero maximo de intentos ${numintentos}`);
+        document.getElementById('nuevo').removeAttribute('disabled');
 
-    if (numUsuario === numeroSecreto){
+    }else if (numUsuario === numeroSecreto){
         mensaje('p',`Acertaste!! Número correcto en ${intentos} ${intentos== 1 ?'intento':'intentos'}`);
         console.log('acertaste el numero es correcto');
+        document.getElementById('nuevo').removeAttribute('disabled');
+
+    }else if (numUsuario > numeroSecreto){
+            mensaje('p',`Sigue intentando. El número es menor al ingresado! Te quedan ${numintentos-intentos} ${intentos == 1 ?'intentos':'intento'}`);
     }else{
-        if (numUsuario > numeroSecreto){
-            mensaje('p','Sigue intentando. El número es menor al ingresado!');
-        }else{
-            mensaje('p','Sigue intentando. El número es mayor al ingresado');
+            mensaje('p',`Sigue intentando. El número es mayor al ingresado! Te quedan ${numintentos-intentos} ${intentos == 1 ?'intentos':'intento'}`);
         }
         intentos++;
-        // sdsada
+        document.querySelector('#valorUsuario').value = '';
     }
+    
+
+
+function limpiarCaja() {
+    let limpio = document.querySelector('#valorUsuario').value = '';
+    let vacio = document.getElementById('valorRango').value = '';
+    return limpio, vacio;    
 }
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    let nuevo = document.querySelector('#nuevo').setAttribute('disabled','true');
+    return nuevo;
+}
+
+condicionesIniciales();
